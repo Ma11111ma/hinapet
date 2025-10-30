2f9d433d16e8e2f633d81b1380f147f50623cad2
Mai <xmax1x27o3@gmail.com>
2025-10-30 20:29:37 +0900

    chore:不要ファイル整理

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  exclude: ["src/_trash/**/*"],
 };
 
 export default nextConfig;
